b2ab4fce541f7eef7f6f6554395dbe2fec641028
Methlodis <methlodis@mysix.com>
2017-05-25 22:09:07 -0400

    Created a Character Template Class
    Created a character template class. This stores and will eventually
    print the final stats of the character template.

---
+++ src/charactertemplate/charactertemplate.java
@@ -0,0 +1,43 @@
+package charactertemplate;
+
+/**
+ * Created by Sterling on 2017-05-25.
+ */
+public class charactertemplate {
+    public static void main(String[] args) {
+        // Character
+        String playername = "Sterling Best";
+        String charactername = "Logan";
+
+        //Roleplay details
+        String alignment = "Nuetral Good";
+        String race = "Human";
+        String size = "Medium";
+        String gender = "Male";
+        int age = 34;
+        int weight = 156;
+        String hair = "Brown";
+        String diety = "God";
+        String homeland = "Canada";
+
+        // Ability Stats
+        int STR = 10;
+        int DEX = 10;
+        int CON = 10;
+        int INT = 10;
+        int WIS = 10;
+        int CHA = 10;
+
+        // Ability Modifiers
+        int strmod = 0;
+        int dexmod = 0;
+        int conmod = 0;
+        int intmod = 0;
+        int wismod = 0;
+        int chamod = 0;
+
+        // A
+
+
+    }
+}
